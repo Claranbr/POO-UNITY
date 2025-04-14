e5794f8d6aaaa100ab28ab95a784530cae4bddf7
Claranbr <clara.nobre@escolar.ifrn.edu.br>
2025-04-14 10:15:05 -0300

    mds

---
+++ POO/Assets/teste.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Teste: MonoBehaviour
+
+Event function 2 Sauto Danit Fetra Punties
+
+void Start()
+
+{
+
+
+    fusca.AtribuirName("Fusca de Pai");
+    fusca.AtribuirCor("Amarelo");
+    fusca.AtribuirNumero_de_rodas(4);
+    fusca.AtribuirPreco (2050.90f);
+    Debug.Log("0"+ fusca.NomeDeCarro()+" é "+ fusca.CorloCarro()
+
+    tem
+
+    for
+
+}
+
+gol.AtribuirName("Gol de Falstão");
+gol.AtribuirCor("Verde");
+gol.AtribuirNumero_de_rodas(4);
