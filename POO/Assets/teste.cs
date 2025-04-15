8148a5410a5aa32f836f54710801b3ca15e9cf24
Claranbr <clara.nobre@escolar.ifrn.edu.br>
2025-04-15 11:09:57 -0300

    deu tudo certo

--- POO/Assets/teste.cs
+++ POO/Assets/teste.cs
@@ -1,26 +1,44 @@
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
-
-public class Teste: MonoBehaviour
-
-Event function 2 Sauto Danit Fetra Punties
-
-void Start()
-
+public class Teste : MonoBehaviour
 {
+    Carro fusca = new Carro();
+    Carro gol = new Carro();
+    
+    //Caminhao
 
+    void Start()
+    {
 
-    fusca.AtribuirName("Fusca de Pai");
-    fusca.AtribuirCor("Amarelo");
-    fusca.AtribuirNumero_de_rodas(4);
-    fusca.AtribuirPreco (2050.90f);
-    Debug.Log("0"+ fusca.NomeDeCarro()+" é "+ fusca.CorloCarro()
+        fusca.AtribuirNome("Fusca de Pai");
+        fusca.AtribuirCor("Amarelo");
+        fusca.AtribuirNumeroDeRodas(4);
+        fusca.AtribuirPreco(2050.90f);
+        Debug.Log("O " + fusca.NomeDoCarro() + " é " + fusca.CorDoCarro() + " ten " + fusca.NumeroDeRodas() +
+                  " e custa R$" + fusca.Preco());
 
-    tem
+        
+        gol.AtribuirNome("Gol do Falstão");
+        gol.AtribuirCor("Verde");
+        gol.AtribuirNumeroDeRodas(4);
+        gol.AtribuirPreco(3000.00f);
+        Debug.Log("O " + gol.NomeDoCarro());
 
-    for
+        if (fusca.ComparPreco(gol))
+        {
+            Debug.Log("O Carro " + fusca.NomeDoCarro() + " é mais caro do que o carro " + gol.NomeDoCarro());
+        }
+        else
+        {
+            Debug.Log("O Carro " + fusca.NomeDoCarro() + " é mais caro do que o carro " + gol.NomeDoCarro());
 
+        }
+    }
+
+    //caminhao.AtribuirTipoDeCarroceria("Larga")
+    //caminhao.AtribuirNumero_de_rodas(8);
+    //caminhao.AtribuirPreco(6000.00f);
+    //caminhao.AtribuirCor("Azul")
+    //caminhao.AtribuirNome("caminhão do falstão");
+        
 }
-
-gol.AtribuirName("Gol de Falstão");
-gol.AtribuirCor("Verde");
-gol.AtribuirNumero_de_rodas(4);
